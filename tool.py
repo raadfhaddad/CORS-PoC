4443fc619a286f53ddf88c02f8bf4da5c67c6d92
Raad <raadfhaddad@gmail.com>
2023-09-04 20:43:42 +0200

    Update tool.py
    Add new options to have better display of the output either detailed or just the results

--- tool.py
+++ tool.py
@@ -7,7 +7,7 @@ TOKEN = "123...XYZ" # ngrok token
 
 def main():
     parser = argparse.ArgumentParser(description="Generate an HTML file to test CORS configurations.")
-    subparsers = parser.add_subparsers(title="subcommands")
+    subparsers = parser.add_subparsers(title="subcommands", dest="subcommands")
 
     parser_ngrok = subparsers.add_parser('ngrok', help="Generate HTML PoC file and run ngrok server")
     parser_ngrok.add_argument('--url', '-u', required=True, help="URL to test for CORS")
@@ -23,18 +23,29 @@ def main():
     
 
     parser_test = subparsers.add_parser('test', help="Test CORS misconfiguration")
-    parser_test.add_argument('--url', '-u', required=True, help="URL to test for CORS misconfigurations")
+    parser_test.add_argument('--url', '-u', help="URL to test for CORS misconfigurations")
+    parser_test.add_argument('--list', '-l', help="Path to a file containing a list of URLs to test. Each URL should be on a new line.")
     parser_test.add_argument('-m', '--method', default='GET', help="HTTP method for the test request. Default is GET.")
     parser_test.add_argument('-H', '--header', action='append', help="Headers to send with the test request. E.g. -H 'Cookie: a=a'")
     parser_test.add_argument('-o', '--origin', action='append', help="Add origin to be used in the Origins list. E.g. -o 'http://web.server'")
+    parser_test.add_argument('-v', '--verbose', action='store_true', help="Show detailed request and response headers")
     parser_test.add_argument('-i', '--ignore404', action='store_true', help="Ignore 404 check")
-    parser_test.set_defaults(func=lambda args: check_cors_vulnerability(args.url, args.method, args.header, args.origin, args.ignore404))  
-
+    parser_test.set_defaults(func=lambda args: check_cors_vulnerability(args.url, args.list, args.method, args.header, args.origin, args.ignore404, args.verbose))  
 
     args = parser.parse_args()
 
+    if args.subcommands == "test":
+        if not args.url and not args.list:
+            print("ERROR: You must provide either --url/-u or --list/-l\n")
+            parser.print_help()
+            exit(1)
+        elif args.url and args.list:
+            print("ERROR: You must provide either --url/-u or --list/-l\n ")
+            parser.print_help()
+            exit(1)
+
     if hasattr(args, 'func'):
-        args.func(args)  
+        args.func(args) 
     else:
         parser.print_help()
 
